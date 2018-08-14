c7fb377d812a4941833936797fd436c1d4c5f2fa
Leonardo De Pinho <ladepinho@gmail.com>
2018-08-14 13:36:18 +0100

    Unit testing services with Jasmine Framework

--- src/app/core/services/data-memory-storage.service.spec.ts
+++ src/app/core/services/data-memory-storage.service.spec.ts
@@ -9,7 +9,17 @@ describe('DataMemoryStorageService', () => {
     });
   });
 
-  it('should be created', inject([DataMemoryStorageService], (service: DataMemoryStorageService) => {
-    expect(service).toBeTruthy();
+  it('saveData() should save key and value in storage', inject([DataMemoryStorageService], (service: DataMemoryStorageService) => {
+    expect(service.saveData('currentMock','mock'));
   }));
+  it('getValue() should return real value', inject([DataMemoryStorageService], (service: DataMemoryStorageService) => {
+    expect(service.getValue('currentMock')).toEqual('mock');
+  }));
+  it('getValue() should return null for not founded keys', inject([DataMemoryStorageService], (service: DataMemoryStorageService) => {
+    expect(service.getValue('currentMock2')).toEqual(null);
+  }));
+  it('removeData() should remove key in storage', inject([DataMemoryStorageService], (service: DataMemoryStorageService) => {
+    expect(service.removeValue('currentMock'));
+  }));
+
 });
--- src/app/core/services/translation.service.spec.ts
+++ src/app/core/services/translation.service.spec.ts
@@ -1,15 +1,43 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TranslationService } from './translation.service';
+import {DataMemoryStorageService} from "./data-memory-storage.service";
 
 describe('TranslationService', () => {
+  let valueServiceSpy: jasmine.SpyObj<DataMemoryStorageService>;
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('DataMemoryStorageService', ['getValue']);
     TestBed.configureTestingModule({
-      providers: [TranslationService]
+      providers: [TranslationService,{ provide: DataMemoryStorageService, useValue: spy }]
     });
   });
 
-  it('should be created', inject([TranslationService], (service: TranslationService) => {
-    expect(service).toBeTruthy();
+
+
+  it('getTranslationProviders() should return value from a promise', inject([TranslationService], (service: TranslationService) => {
+    (done: DoneFn) => {
+      service.getTranslationProviders().then(value => {
+        expect(value).toBe('promise value');
+        done();
+      });
+    }
   }));
+  it('getFile() should return value from a promise', inject([TranslationService], (service: TranslationService) => {
+    (done: DoneFn) => {
+      service.getFile('mock').then(value => {
+        expect(value).toBe('promise value');
+        done();
+      });
+    }
+  }));
+  it('setLenguages() should return array of lenguages', inject([TranslationService], (service: TranslationService) => {
+    expect(service.setLenguages()).toEqual([ 'english', 'spanish' ]);
+  }));
+  it('setCulture() should return real value', inject([TranslationService], (service: TranslationService) => {
+    expect(service.setCulture()).toEqual('us');
+  }));
+  it('getLocale() should return real value', inject([TranslationService], (service: TranslationService) => {
+    expect(service.getLocale('us','english')).toEqual('en-US');
+  }));
+
 });
